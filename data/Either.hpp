83f7d7e2e35edbc172247b7de9afbb7d5f37426c
Clinton Selke <clinuxrulz@gmail.com>
2016-11-16 20:45:38 +1000

    either of non-trival classes while using stack space. (had to be wasteful with memory)

--- data/Either.hpp
+++ data/Either.hpp
@@ -7,41 +7,34 @@
 namespace highcpp_data {
 
   template <typename A, typename B>
-  union __EitherValue {
-    A leftValue;
-    B rightValue;
-  };
-
-  template <typename A, typename B>
   class Either {
   public:
 
     static Either<A,B> left(A a) {
-      __EitherValue<A,B> value;
-      value.leftValue = a;
-      return Either(false, value);
+      B dummy;
+      return Either(false, a, dummy);
     }
 
     static Either<A,B> right(B b) {
-      __EitherValue<A,B> value;
-      value.rightValue = b;
-      return Either(true, value);
+      A dummy;
+      return Either(true, dummy, b);
     }
 
     template <typename C>
     C cata(std::function<C(const A&)> kLeft, std::function<C(const B&)> kRight) const {
       if (isRight) {
-        return kRight(value.rightValue);
+        return kRight(rightValue);
       } else {
-        return kLeft(value.leftValue);
+        return kLeft(leftValue);
       }
     }
 
   private:
     bool isRight;
-    __EitherValue<A,B> value;
+    A leftValue;
+    B rightValue;
 
-    Either(bool isRight, __EitherValue<A,B> value): isRight(isRight), value(value) {}
+    Either(bool isRight, A leftValue, B rightValue): isRight(isRight), leftValue(leftValue), rightValue(rightValue) {}
   };
 
   template <typename A, typename B>
