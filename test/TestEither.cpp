4aad14bfa6647f1a9f01b4657c43546ee3cebe94
Clinton Selke <clinuxrulz@gmail.com>
2016-11-16 20:43:33 +1000

    either of non-trival classes while using stack space. (had to be wasteful with memory)

--- test/TestEither.cpp
+++ test/TestEither.cpp
@@ -1,10 +1,40 @@
 #include "../data/Either.hpp"
 
+#include <memory>
+
 using namespace std;
 using namespace highcpp_data;
 
+class MyClassA {
+public:
+  MyClassA() {
+    cout << "MyClassA constructor.";
+  }
+  virtual ~MyClassA() {
+    cout << "MyClassA destructor.";
+  }
+};
+
+class MyClassB {
+public:
+  MyClassB() {
+    cout << "MyClassB constructor.";
+  }
+  virtual ~MyClassB() {
+    cout << "MyClassB destructor.";
+  }
+};
+
+static ostream& operator<<(ostream& os, const MyClassA& a) {
+  return os << "MyClassA";
+}
+
+static ostream& operator<<(ostream& os, const MyClassB& b) {
+  return os << "MyClassB";
+}
+
 int main() {
-  Either<int,float> x = Either<int,float>::right(6.2f);
-  cout << x;
+  auto x = Either<MyClassA,MyClassB>::right(MyClassB());
+  cout << x << endl;
   return 0;
 }
