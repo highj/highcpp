2233e2e9a56b80ec657cbcc3f3bffa7e79974efa
Clinton Selke <clinuxrulz@gmail.com>
2016-11-19 18:42:14 +1000

    wip MemPool

--- mem/mem_pool.hpp
+++ mem/mem_pool.hpp
@@ -11,38 +11,32 @@ struct MemPoolRef {
 template <typename A>
 struct MemPool {
   static A* pool = nullptr;
-  static int freeIndex = 0;
-  static int size = 0;
-  static const int memPoolInitSize = 64;
-
-  static initMemPool() {
-    pool = (A*)malloc(sizeof(A) * memPoolInitSize);
-    size = memPoolInitSize;
-    freeIndex = 0;
-  }
-
-  static makeMemPoolLarger() {
-    int newSize = size * 2;
-    pool = (A*)realloc(sizeof(A) * newSize);
-    size = newSize;
-  }
+  static int* freeIndices = nullptr;
+  static int poolCapacity = 0;
+  static int poolSize = 0;
+  static int freeIndicesCapacity = 0;
+  static inf freeIndicesSize = 0;
+  static const int poolInitSize = 64;
+  static const int freeIndicesInitSize = 64;
 
   static MemPoolRef<A> alloc(A a) {
     if (pool == nullptr) { initMemPool(); }
-    if (freeIndex == size) { makeMemPoolLarger(); }
-    MemPoolRef<A> ref;
-    ref.index = freeIndex;
-    pool[ref.index] = a;
-    ++freeIndex;
-    return ref;
+    if (hasFreeIndex()) {
+      MemPoolRef<A> ref;
+      ref.index = popFreeIndex();
+      pool[ref.index] = a;
+      return ref;
+    } else {
+      if (poolSize == poolCapacity) { makeMemPoolLarger(); }
+      MemPoolRef<A> ref;
+      ref.index = poolSize++;
+      pool[ref.index] = a;
+      return ref;
+    }
   }
 
   static void free(MemPoolRef<A> ref) {
-    if (ref.index == freeIndex-1) {
-      --freeIndex;
-    } else {
-      // TODO: Finish this. May need a 2nd array for index remapping.
-    }
+    pushFreeIndex(ref.index);
   }
 
   static A read(MemPoolRef<A> ref) {
@@ -51,6 +45,42 @@ struct MemPool {
 
   static void write(MemPoolRef<A> ref, A value) {
     pool[ref.index] = value;
+  }
+
+private:
+  static void initMemPool() {
+    pool = (A*)malloc(sizeof(A) * poolInitSize);
+    freeIndices = (int*)malloc(sizeof(int) * freeIndicesInitSize);
+    poolCapacity = memPoolInitSize;
+    freeIndicesCapacity = freeIndicesInitSize;
+    freeIndex = 0;
+  }
+
+  static void makeMemPoolLarger() {
+    int newPoolCapacity = poolCapacity * 2;
+    pool = (A*)realloc(sizeof(A) * newPoolCapacity);
+    poolCapacity = newPoolCapacity;
+  }
+
+  static void makeFreeIndicesLarger() {
+    int newFreeIndicesCapacity = freeIndicesCapacity * 2;
+    freeIndices = (int*)realloc(sizeof(int) * newFreeIndicesCapacity);
+    freeIndicesCapacity = newFreeIndicesCapacity;
+  }
+
+  static bool hasFreeIndex() {
+    return freeIndicesSize > 0;
+  }
+
+  static void pushFreeIndex(int freeIndex) {
+    if (freeIndicesSize == freeIndicesCapacity) {
+      makeFreeIndicesLarger();
+    }
+    freeIndices[freeIndicesSize++] = freeIndex;
+  }
+
+  static int popFreeIndex() {
+    return freeIndices[--freeIndicesSize];
   }
 }
 
